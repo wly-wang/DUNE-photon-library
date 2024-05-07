18e487207063f795d24a8f679bb31963c672d36e
bwynne <bwynne@t3-mw2.ph.ed.ac.uk>
2024-05-07 14:25:58 +0100

    No-build init

--- initialise.sh
+++ initialise.sh
@@ -5,13 +5,4 @@ export MY_DUNE_VERSION=v09_90_00
 
 # Env setup
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-setup dunesw ${MY_DUNE_VERSION}d00 -q e20:prof
-
-# Activate build area if there is one
-source $PWD/localProducts_larsoft_*/setup
-
-# Build
-mrbsetenv
-mrb i -j12
-mrbslp
-
+setup dunesw ${MY_DUNE_VERSION}d00 -q e26:prof
