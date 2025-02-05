106f87ab305a65179c32be19d4bf72d2ec5316f4
William Wang <wwang@dunegpvm11.fnal.gov>
2025-02-05 08:43:12 -0600

    half-made changes to adapt protodune build_optical_lib_fcl to modded_fd1_hd_1x2x6

--- initialise.sh
+++ initialise.sh
@@ -1,7 +1,7 @@
 # Choose dune sw version
 # LArSim from LARSOFT_SUITE_v09_90_00 or newer contains the automatic voxelisation from https://github.com/LArSoft/larsim/pull/135
 ls /cvmfs/dune.opensciencegrid.org/products/dune/dunesw/
-export MY_DUNE_VERSION=v09_90_00
+export MY_DUNE_VERSION=v10_02_02
 
 # Env setup
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
